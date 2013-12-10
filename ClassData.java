9fdb1ee535960a85593aa3a29a0d149c61b47da9
Chris <c5keith90@gmail.com>
2013-12-10 09:06:02 -0700

    Done

--- ClassData.java
+++ ClassData.java
@@ -28,7 +28,13 @@ public class ClassData{
                 
         System.out.print(" What would you like to do? ");
         selectMyString = classRead.nextLine();
+        try{
         selectMyItem = Integer.parseInt(selectMyString);
+        }
+        catch(NumberFormatException e) {
+            System.out.println("\n\tYou have entered an incorrect value.\n If I sent money to you, I am pretty sure I could write it off as charity work.\n");
+            selectMyItem=4;
+        }
         if(selectMyItem>5)
             {
                 System.out.println("\tPlease select correct number.");
@@ -44,20 +50,32 @@ public class ClassData{
                         break;
                        case 3:
                         System.out.println();
-                        System.out.println("Group students by:");
-                        System.out.println("\tPress [2] to group by two.");
-                        System.out.println("\tPress [3] to group by three.");
-                        System.out.println("\tPress [4] to group by four.");
-                        System.out.println("\tPress [5] to group by five.");
-                        System.out.println("\tPress [6] to group by six.");
-                        System.out.println("\tPress [7] to group by seven.");
-                        System.out.println("\tPress [8] to group by four.");
-                        System.out.println("\tPress [9] to group by five.");
-                        System.out.print(" Group by what number? ");
+                        // System.out.println("Group students by:");
+                        // System.out.println("\tPress [2] to group by two.");
+                        // System.out.println("\tPress [3] to group by three.");
+                        // System.out.println("\tPress [4] to group by four.");
+                        // System.out.println("\tPress [5] to group by five.");
+                        // System.out.println("\tPress [6] to group by six.");
+                        // System.out.println("\tPress [7] to group by seven.");
+                        // System.out.println("\tPress [8] to group by four.");
+                        // System.out.println("\tPress [9] to group by five.");
+                        System.out.print(" How many people per-groups do you want? ");
                         String newGroup = classRead.nextLine();
+                        try {
                         int groupNo = Integer.parseInt(newGroup);
-                        System.out.println(groupNo);
-                        myClassData.groupStudents(groupNo);
+                            if (groupNo > myClassData.studentNumber()) {
+                            System.out.println("\tWarning you've chosen a number that is larger\n\t than the class size.\n");
+                            }else{
+                                if ((double)myClassData.studentNumber()/groupNo < 1.9) {
+                                    System.out.println("\tWarning you've selected to group by a number \n\t that is more than half the class size.\n");
+                                }
+                                myClassData.groupStudents(groupNo);
+                            }
+                        }
+                        catch(NumberFormatException e) {
+                            System.out.println("You have entered an incorect value. You can try to get it right again or give up. ");  
+                            return;
+                        }
                         break;
                         default:
                         System.out.print("\tProgram exited.");
--- studentData.java
+++ studentData.java
@@ -40,39 +40,49 @@ public class studentData {
 		return studentName.length;
 	}
 	public void groupStudents(int x){
-	String [] shuffledStudents = (String[])studentName.clone();
-	int noOfGroups = shuffledStudents.length/x;
-	int groupsPlusOne = 0;
-	if (shuffledStudents.length%x > 0) {
-		groupsPlusOne = shuffledStudents.length%x;
-		System.out.println("Number of Groups = "+noOfGroups+" Groups that need one more "+ groupsPlusOne);	
-	}
-	String tempName;
-	for (int i=shuffledStudents.length-1;i>=0;i--) {
-		int rand = (int)(Math.random()*(i+1));
-		tempName = shuffledStudents[i];
-		shuffledStudents[i] = shuffledStudents[rand];
-		shuffledStudents[rand] = tempName;
-	}
-	int arrayCounter = 0;
-	System.out.println();
-	for (int i = 1; i <=noOfGroups; i++) {
-		System.out.print(" Group "+ i+ " " );
-		int t=0;
-			if (i <=groupsPlusOne) {
-				t=x+1;
-			}
-				else {
-				t=x;
-				}
-		for (int j = 1;j <= t ;j++) {
-			System.out.print(shuffledStudents[arrayCounter]);
-			arrayCounter++;
-			if (i <=noOfGroups && j< t) {
-				System.out.print(", ");
-			}
-		}
-	System.out.println();	
-	}
-	}
+	   String [] shuffledStudents =  (String[])studentName.clone();
+    int noOfGroups = shuffledStudents.length/x;
+    int groupsPlusOne = 0;
+    int noToAdd = 0;
+    boolean addGroup = false;
+    if (shuffledStudents.length%x > 0) {
+        groupsPlusOne = shuffledStudents.length%x;
+         if (((double)(groupsPlusOne)/x)> .5){
+            // System.out.println("groupsPlusOne/x = "+((double)(groupsPlusOne)/x));
+             noOfGroups+=1;  
+             addGroup = true; 
+             groupsPlusOne = 0;
+            }else if(((double)(groupsPlusOne)/x)< .5 && groupsPlusOne>0){
+                noToAdd = groupsPlusOne/noOfGroups;
+            }
+         // System.out.println("No of Groups = "+noOfGroups+" Groups that need one more: "+ groupsPlusOne);
+    }
+      String tempName;
+    for (int i = shuffledStudents.length-1; i >= 0; i--){
+           int rand = (int)(Math.random()*(i+1));
+            tempName = shuffledStudents[i];
+            shuffledStudents[i] = shuffledStudents[rand];
+            shuffledStudents[rand] = tempName;
+    }
+    int arrayCounter = 0;
+    System.out.println();
+   for (int i = 1; i <= noOfGroups; i++){
+     System.out.print(" Group "+ i+ " " );
+     int t = x+noToAdd;
+     if (i == 1 && addGroup==false && groupsPlusOne > 0 && groupsPlusOne%noOfGroups > 0){
+        t+=1;
+     }
+     
+     for (int j = 1; j<= t; j++){
+        if(arrayCounter < shuffledStudents.length){
+            System.out.print(shuffledStudents[arrayCounter]);
+            arrayCounter++;
+            if (i<=noOfGroups && j< t && arrayCounter < shuffledStudents.length){
+                System.out.print(", ");
+            }   
+        }
+     }
+     System.out.println();
+   }
+ }
 }
